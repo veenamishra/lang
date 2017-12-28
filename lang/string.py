af1984f0326003427d74b0161e0a8120c204d490
veenamishra <mail2veenamishra@gmail.com>
2017-12-27 19:10:34 -0800

    Add files via upload

--- lang/string.py
+++ lang/string.py
@@ -1,1 +1,29 @@
-# print ("hello veena")
+message = 'Hello World'
+print (message[6:11]) ##Slicing
+
+print(message.lower())
+print(message.count('l'))
+print(message.find('World'))
+print(message.find('Universe'))
+
+new_message = message.replace('World', 'Universe')
+print(new_message)
+
+greeting = 'Hello'
+name = 'Michael'
+
+message = greeting + ', ' + name
+
+print(message)
+
+print(dir(name))
+
+upper, lower, find, replace, format, f
+
+
+
+
+
+
+
+
